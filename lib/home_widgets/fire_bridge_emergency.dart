68e0a700dc6c7f105008422ee1d97f23ae146def
Ansooman <ansooman21@gmail.com>
2023-04-24 01:40:32 +0530

    calling_fuction

--- lib/home_widgets/fire_bridge_emergency.dart
+++ lib/home_widgets/fire_bridge_emergency.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
 
 class FireBrigadeEmergency extends StatelessWidget {
+  _callNumber(String number) async {
+    await FlutterPhoneDirectCaller.callNumber(number);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -14,72 +19,75 @@ class FireBrigadeEmergency extends StatelessWidget {
             30,
           ),
         ),
-        child: Container(
-          height: 100,
-          width: MediaQuery.of(context).size.width * 0.7,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                Color.fromRGBO(242, 144, 167, 1),
-                Color.fromRGBO(242, 144, 167, 1),
-                Color.fromRGBO(236, 209, 215, 1),
-              ],
+        child: InkWell(
+          onTap: () => _callNumber('102'),
+          child: Container(
+            height: 100,
+            width: MediaQuery.of(context).size.width * 0.7,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color.fromRGBO(242, 144, 167, 1),
+                  Color.fromRGBO(242, 144, 167, 1),
+                  Color.fromRGBO(236, 209, 215, 1),
+                ],
+              ),
             ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                CircleAvatar(
-                  radius: 25,
-                  backgroundColor: Colors.white.withOpacity(0.5),
-                  child: Image.asset(
-                    "assets/firetruck.png",
-                    height: 40,
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    radius: 25,
+                    backgroundColor: Colors.white.withOpacity(0.5),
+                    child: Image.asset(
+                      "assets/firetruck.png",
+                      height: 40,
+                    ),
                   ),
-                ),
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Active Emergency",
-                        style: TextStyle(
-                            fontSize: 25,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white),
-                      ),
-                      Text(
-                        "Call 1-0-2 for emergency",
-                        style: TextStyle(fontSize: 15, color: Colors.white),
-                      ),
-                      Container(
-                        height: 30,
-                        width: 80,
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(30)),
-                        child: Center(
-                          child: Text(
-                            "1-0-2",
-                            style: TextStyle(
-                              fontSize: 18,
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "FireBrigade Emergency",
+                          style: TextStyle(
+                              fontSize: 22,
                               fontWeight: FontWeight.bold,
-                              color: Color.fromRGBO(217, 98, 98, 1),
+                              color: Colors.white),
+                        ),
+                        Text(
+                          "Call 1-0-2 for emergency",
+                          style: TextStyle(fontSize: 15, color: Colors.white),
+                        ),
+                        Container(
+                          height: 30,
+                          width: 80,
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(30)),
+                          child: Center(
+                            child: Text(
+                              "1-0-2",
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromRGBO(217, 98, 98, 1),
+                              ),
                             ),
                           ),
-                        ),
-                      )
-                    ],
+                        )
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
