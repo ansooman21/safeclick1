184055dc682b20b3cb199dc85fd543982df6c109
Ansooman <ansooman21@gmail.com>
2023-04-24 01:19:34 +0530

    slider

---
+++ lib/home_widgets/emergency.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:safeclick1/home_widgets/policeEmergency.dart';
+
+import 'ambulance_emergency.dart';
+import 'army_emergency.dart';
+import 'fire_bridge_emergency.dart';
+
+class emergency extends StatelessWidget {
+  const emergency({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 180,
+      child: ListView(
+        physics: BouncingScrollPhysics(),
+        scrollDirection: Axis.horizontal,
+        children: [
+          policeEmergency(),
+          AmbulanceEmergency(),
+          FireBrigadeEmergency(),
+          ArmyEmergency()
+        ],
+      ),
+    );
+  }
+}
