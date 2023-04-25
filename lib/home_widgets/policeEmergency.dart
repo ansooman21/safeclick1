6c78a4632669c4a7bb846cb0b3682378b9a41b68
Ansooman <ansooman21@gmail.com>
2023-04-26 00:03:02 +0530

    changes..now

--- lib/home_widgets/policeEmergency.dart
+++ lib/home_widgets/policeEmergency.dart
@@ -20,7 +20,7 @@ class policeEmergency extends StatelessWidget {
           ),
         ),
         child: InkWell(
-          onTap: () => _callNumber('101'),
+          onTap: () => _callNumber('100'),
           child: Container(
             height: 100,
             width: MediaQuery.of(context).size.width * 0.7,
